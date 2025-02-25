1760a9a9b667ae0c6e67b7c4c7e1b90d406be8b3
Connorblu <cablumsack@gmail.com>
2025-02-25 18:41:15 -0500

    Adding default connection values for testing

--- src/recommenderapp/app.py
+++ src/recommenderapp/app.py
@@ -601,9 +601,9 @@ def before_request():
     """
     load_dotenv()
     g.db = mysql.connector.connect(
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER", "root"),
+        password=os.getenv("DB_PASSWORD", "root"),
+        host=os.getenv("DB_HOST", "db"),
         port=os.getenv("DB_PORT", 3306),
         database=os.getenv("DB_NAME"),
     )
