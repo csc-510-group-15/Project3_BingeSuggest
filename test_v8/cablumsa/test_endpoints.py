ccac63189110ffabd411683ab46b142b5e894c27
GitHub Action <action@github.com>
2025-02-25 01:33:32 +0000

    Automatically format code with Black

--- test_v8/cablumsa/test_endpoints.py
+++ test_v8/cablumsa/test_endpoints.py
@@ -3,10 +3,12 @@ import json
 import sys
 from pathlib import Path
 import os
+
 sys.path.append(str(Path(__file__).resolve().parents[2]))
 # pylint: disable=wrong-import-position
 
 from src.recommenderapp.app import app
+
 
 class TestApp(unittest.TestCase):
     def setUp(self):
@@ -14,19 +16,20 @@ class TestApp(unittest.TestCase):
         self.app.testing = True
 
     def test_login_page(self):
-        response = self.app.get('/')
+        response = self.app.get("/")
         self.assertEqual(response.status_code, 200)
-        self.assertIn(b'login', response.data)
+        self.assertIn(b"login", response.data)
 
     def test_predict_g(self):
-        data = {
-            "movie_list": ["Inception", "The Matrix"]
-        }
-        response = self.app.post('/genreBased', data=json.dumps(data), content_type='application/json')
+        data = {"movie_list": ["Inception", "The Matrix"]}
+        response = self.app.post(
+            "/genreBased", data=json.dumps(data), content_type="application/json"
+        )
         self.assertEqual(response.status_code, 200)
-        self.assertIn('recommendations', response.json)
-        self.assertIn('genres', response.json)
-        self.assertIn('imdb_id', response.json)
+        self.assertIn("recommendations", response.json)
+        self.assertIn("genres", response.json)
+        self.assertIn("imdb_id", response.json)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
