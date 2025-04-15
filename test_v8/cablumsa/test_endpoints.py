690dd2f89f8c2d88d8112c34b768665ec718fed6
GitHub Action <action@github.com>
2025-04-15 13:02:00 +0000

    Automatically format code with Black

--- test_v8/cablumsa/test_endpoints.py
+++ test_v8/cablumsa/test_endpoints.py
@@ -13,6 +13,7 @@ sys.path.insert(0, str(Path(__file__).resolve().parents[2]))
 
 from src.recommenderapp.app import app
 
+
 # --- Patch MySQL Connector to avoid real DB calls ---
 class DummyCursor:
     def execute(self, *args, **kwargs):
@@ -59,10 +60,12 @@ def client():
 #         self.app = app.test_client()
 #         self.app.testing = True
 
+
 def test_login_page(client):
     response = client.get("/")
-    assert (response.status_code == 200)
-    assert (b"login" in response.data)
+    assert response.status_code == 200
+    assert b"login" in response.data
+
 
 # In other test files we have tested that the recommendation algorithms work as expected.
 # Now, we want to test that the endpoints are outputting that recommendation in the proper format,
@@ -72,68 +75,76 @@ def test_predict_g(client):
     response = client.post(
         "/genreBased", data=json.dumps(data), content_type="application/json"
     )
-    assert (response.status_code == 200)
-    assert ("recommendations" in response.json)
-    assert ("genres" in response.json)
-    assert ("imdb_id" in response.json)
+    assert response.status_code == 200
+    assert "recommendations" in response.json
+    assert "genres" in response.json
+    assert "imdb_id" in response.json
+
 
 def test_predict_g_invalid(client):
     data = {"movie_list": []}
     response = client.post(
         "/genreBased", data=json.dumps(data), content_type="application/json"
     )
-    assert (response.status_code == 400)
+    assert response.status_code == 400
+
 
 def test_predict_g_invalid_2(client):
     data = {"something_invalid": "test"}
     response = client.post(
         "/genreBased", data=json.dumps(data), content_type="application/json"
     )
-    assert (response.status_code == 400)
+    assert response.status_code == 400
+
 
 def test_predict_d(client):
     data = {"movie_list": ["Inception", "The Matrix"]}
     response = client.post(
         "/dirBased", data=json.dumps(data), content_type="application/json"
     )
-    assert (response.status_code == 200)
-    assert ("recommendations" in response.json)
-    assert ("genres" in response.json)
-    assert ("imdb_id" in response.json)
+    assert response.status_code == 200
+    assert "recommendations" in response.json
+    assert "genres" in response.json
+    assert "imdb_id" in response.json
+
 
 def test_predict_d_invalid(client):
     data = {"movie_list": []}
     response = client.post(
         "/dirBased", data=json.dumps(data), content_type="application/json"
     )
-    assert (response.status_code == 400)
+    assert response.status_code == 400
+
 
 def test_predict_a(client):
     data = {"movie_list": ["Inception", "The Matrix"]}
     response = client.post(
         "/actorBased", data=json.dumps(data), content_type="application/json"
     )
-    assert (response.status_code == 200)
-    assert ("recommendations" in response.json)
-    assert ("genres" in response.json)
-    assert ("imdb_id" in response.json)
+    assert response.status_code == 200
+    assert "recommendations" in response.json
+    assert "genres" in response.json
+    assert "imdb_id" in response.json
+
 
 def test_predict_a_invalid(client):
     data = {"movie_list": []}
     response = client.post(
         "/actorBased", data=json.dumps(data), content_type="application/json"
     )
-    assert (response.status_code == 400)
+    assert response.status_code == 400
+
 
 def test_predict_all(client):
     data = {"movie_list": ["Inception", "The Matrix"]}
     response = client.post(
         "/all", data=json.dumps(data), content_type="application/json"
     )
-    assert (response.status_code == 200)
-    assert ("recommendations" in response.json)
-    assert ("genres" in response.json)
-    assert ("imdb_id" in response.json)
+    assert response.status_code == 200
+    assert "recommendations" in response.json
+    assert "genres" in response.json
+    assert "imdb_id" in response.json
+
 
 # Ensure that passing an empty list is impossible, and should return an error code
 def test_predict_all_invalid(client):
@@ -141,62 +152,70 @@ def test_predict_all_invalid(client):
     response = client.post(
         "/all", data=json.dumps(data), content_type="application/json"
     )
-    assert (response.status_code == 400)
+    assert response.status_code == 400
+
 
 def test_create_account_invalid(client):
     data = {"an_invalid_field": "test"}
-    response = client.post(
-        "/", data=json.dumps(data), content_type="application/json"
-    )
-    assert (response.status_code == 400)
+    response = client.post("/", data=json.dumps(data), content_type="application/json")
+    assert response.status_code == 400
+
 
 def test_guest(client):
     data = {"guest": "guest"}
     response = client.post(
         "/guest", data=json.dumps(data), content_type="application/json"
     )
-    assert (response.status_code == 200)
+    assert response.status_code == 200
+
 
 def test_guest_invalid(client):
     data = {"not_guest": "not_guest"}
     response = client.post(
         "/guest", data=json.dumps(data), content_type="application/json"
     )
-    assert (response.status_code == 400)
+    assert response.status_code == 400
+
 
 def test_guest_empty(client):
     data = {}
     response = client.post(
         "/guest", data=json.dumps(data), content_type="application/json"
     )
-    assert (response.status_code == 400)
+    assert response.status_code == 400
+
 
 # We need to test that the movie page is rendering
 # Let's make sure that the movie page works with multiple differemt movies
 def test_render_movie(client):
     response = client.get("/movie/tt0376994")
-    assert (response.status_code == 200)
-    assert (b"X-Men" in response.data)
+    assert response.status_code == 200
+    assert b"X-Men" in response.data
+
 
 def test_render_movie_2(client):
     response = client.get("/movie/tt1430132")
-    assert (response.status_code == 200)
-    assert (b"The Wolverine" in response.data)
+    assert response.status_code == 200
+    assert b"The Wolverine" in response.data
+
 
 def test_render_movie_3(client):
     response = client.get("/movie/tt0145487")
-    assert (response.status_code == 200)
-    assert (b"Spider-Man" in response.data)
+    assert response.status_code == 200
+    assert b"Spider-Man" in response.data
+
 
 # Now we can test some invalid imdb_ids
 def test_render_movie_invalid(client):
     response = client.get("/movie/tt")
-    assert (response.status_code == 404)
+    assert response.status_code == 404
+
 
 def test_render_movie_invalid_2(client):
     response = client.get("/movie/")
-    assert (response.status_code == 404)
+    assert response.status_code == 404
+
 
 def test_render_movie_invalid_3(client):
     response = client.get("/movie/1")
-    assert (response.status_code == 404)
+    assert response.status_code == 404
